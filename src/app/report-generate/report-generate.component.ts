044c85d8a0c7828d1a9cc0636a0905ab50a37bbf
MrPaul98 <sayandippaul1998@gmail.com>
2023-11-27 20:18:57 +0530

    new update

--- src/app/report-generate/report-generate.component.ts
+++ src/app/report-generate/report-generate.component.ts
@@ -1,3 +1,39 @@
+export interface GuestElement {
+  id: number;
+  name: string;
+  designation: string;
+  contact: number;
+  checkinTime: string,
+  checkinTrain: number,
+  checkoutTime: string,
+  checkoutTrain: number,
+  room: string,
+}
+const ELEMENT_DATA: GuestElement[] = [
+  {
+    id: 1121,
+    name: 'Sarkar',
+    designation: 'Loco Pilot',
+    contact: 9199123131,
+    checkinTime: '23-11-2023 06:00',
+    checkinTrain: 12378,
+    checkoutTime: '24-11-2023 18:00',
+    checkoutTrain: 22501,
+    room: '101 Bed-1',
+  },
+  {
+    id: 1125,
+    name: 'Ghosh',
+    designation: 'Staff',
+    contact: 9123198231,
+    checkinTime: '21-11-2023 12:39',
+    checkinTrain: 12770,
+    checkoutTime: '21-11-2023 21:25',
+    checkoutTrain: 12791,
+    room: '102 Bed-2',
+  },
+];
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
 @Component({
@@ -8,31 +44,23 @@ import * as XLSX from 'xlsx';
 export class ReportGenerateComponent implements OnInit {
   fileName = 'Report.xlsx';
   constructor() {}
-  guestList = [
-    {
-      id: 1121,
-      name: 'Sarkar',
-      designation: 'Loco Pilot',
-      contact: 9199123131,
-      checkin_time: '23-11-2023 22:39',
-      checkin_train: 12378,
-      checkout_time: '24-11-2023 06:15',
-      checkout_train: 22501,
-      room: '101 Bed-1',
-    },
-    {
-      id: 1125,
-      name: 'Ghosh',
-      designation: 'Staff',
-      contact: 9123198231,
-      checkin_time: '21-11-2023 12:39',
-      checkin_train: 12770,
-      checkout_time: '21-11-2023 21:25',
-      checkout_train: 12791,
-      room: '102 Bed-2',
-    },
-  ];
+  fromDate: Date | undefined;
+  displayedColumns: string[] = [
+  'id',
+  'name',
+  'designation',
+  'contact',
+  'checkinTime',
+  'checkinTrain',
+  'checkoutTime',
+  'checkoutTrain',
+  'room'];
+  guestList = ELEMENT_DATA;
   ngOnInit(): void {}
+  checkDate(){
+    console.log(this.fromDate);
+    console.log(this.guestList.filter(e => e.name === 'Ghosh'));
+  }
   export() {
     let element = document.getElementById('report-table');
     const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
